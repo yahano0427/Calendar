079c1c7512418cd2bfbc78b6f1d1dcf78675a993
sasataniryota <61574281+sasataniryota@users.noreply.github.com>
2020-08-06 14:23:53 +0900

    firebase初期設定

--- Calendar/AppDelegate.swift
+++ Calendar/AppDelegate.swift
@@ -1,4 +1,4 @@
-//
+
 //  AppDelegate.swift
 //  Calendar
 //
@@ -7,17 +7,20 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    //Firebase接続設定
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //Firebase接続設定
+        FirebaseApp.configure()
+        
         return true
     }
-
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+ platform :ios, '9.0'
 
 target 'Calendar' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -8,6 +8,12 @@ target 'Calendar' do
   #Calenderに必要なライブラリ
   pod 'FSCalendar'
   pod 'CalculateCalendarLogic'
+
+  # Firebaseにあったものをコピペ
+  # add the Firebase pod for Google Analytics
+  pod 'Firebase/Analytics'
+  # add pods for any other desired Firebase products
+  # https://firebase.google.com/docs/ios/setup#available-pods
 
   # Pods for Calendar
 
