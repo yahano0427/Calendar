6a30de11be44a95d1abfb0f1f8ff8fd2082f5182
ryota917 <ryotadegozaru917@icloud.com>
2020-08-14 14:33:20 +0900

    comment

--- Calendar/SettingViewController.swift
+++ Calendar/SettingViewController.swift
@@ -11,13 +11,61 @@ import FirebaseFirestore
 import FirebaseUI
 
 class SettingViewController: UIViewController, UITextFieldDelegate {
+    //ログインユーザー
+    let currentUser = Auth.auth().currentUser
+    
+    //フォローまたはフォロワーのユーザーデータを格納する
+    var searchedUsers = [Dictionary<String, Any>]()
     
     @IBOutlet weak var nameTextField: UITextField!
     
     var authUI: FUIAuth { get { return FUIAuth.defaultAuthUI()! }}
     
     @IBAction func followButton(_ sender: Any) {
-        
+        //フォローしているユーザーデータを取得してShowFollow UserControllerに画面遷移する
+        Firestore.firestore().collection("users").document(currentUser!.uid).collection("follow").getDocuments() {(QuerySnapshot, err) in
+            if let err = err {
+                print("ログインユーザーのフォローデータ取得時にエラーが発生しました")
+            } else {
+                for document in QuerySnapshot!.documents {
+                    self.searchedUsers.append(document.data())
+                    print("ログインユーザーのフォローデータ取得に成功しました:\(document.data())")
+                }
+            }
+            
+            //フォロー一覧画面に遷移
+            //Firestoreの処理内に記述することがポイント、そうしないと非同期処理で先にperformSegueが呼ばれるために、データ渡すことができない
+            self.performSegue(withIdentifier: "showFollowSegue", sender: self)
+        }
+    }
+    
+    @IBAction func tap(_ sender: Any) {
+        self.performSegue(withIdentifier: "test", sender: self)
+    }
+    
+    @IBAction func followerButton(_ sender: Any) {
+        //フォロワーのユーザーデータを取得してShowFolloUserControllerに画面遷移
+        Firestore.firestore().collection("users").document(currentUser!.uid).collection("follower").getDocuments() {(QuerySnapshot, err) in
+            if let err = err {
+                print("ログインユーザーのフォロワーデータ取得時にエラーが発生しました")
+            } else {
+                for document in QuerySnapshot!.documents {
+                    self.searchedUsers.append(document.data())
+                    print("ログインユーザーのフォロワーデータ取得に成功しました:\(document.data())")
+                }
+            }
+            
+            //フォロワー一覧画面に遷移
+            //Firestoreの処理内に記述することがポイント、そうしないと非同期処理で先にperformSegueが呼ばれるために、データ渡すことができない
+            self.performSegue(withIdentifier: "showFollowSegue", sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showFollowSegue" {
+            let nextVC = segue.destination as! ShowFollowUserViewController
+            nextVC.searchedUsers = self.searchedUsers
+        }
     }
     
     @IBAction func changeProfile(_ sender: Any) {
